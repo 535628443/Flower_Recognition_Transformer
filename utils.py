2c2312f1487e4b66777022c7576e90c780cca082
535628443 <535628443@qq.com>
2026-03-17 23:54:34 +0800

    实现了loss和accuracy曲线的绘制函数plot_loss_curves

--- utils.py
+++ utils.py
@@ -16,3 +16,37 @@ def save_model(model, target_dir, model_name):
     print(f"[INFO] 正在保存模型到: {model_save_path}")
     # 保存
     torch.save(obj=model.state_dict(), f=model_save_path)
+
+# 绘制loss和accuracy曲线
+def plot_loss_curves(results):
+    loss = results['train_loss']
+    test_loss = results['val_loss']
+    accuracy = results['train_acc']
+    test_accuracy = results['val_acc']
+    epochs = range(len(results['train_loss']))
+
+    plt.figure(figsize=(15, 7))
+
+    # 绘制 Loss 曲线
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs, loss, label='train_loss')
+    plt.plot(epochs, test_loss, label='val_loss')
+    plt.title('Training and Validation Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+
+
+    # 绘制 Accuracy 曲线
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs, accuracy, label='train_accuracy')
+    plt.plot(epochs, test_accuracy, label='val_accuracy')
+    plt.title('Training and Validation Accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+ 
+    # 确保保存目录存在
+    os.makedirs("results", exist_ok=True)
+    plt.savefig('results/loss_curves.png')
+    print("[INFO] 训练曲线已保存至 results/loss_curves.png")
